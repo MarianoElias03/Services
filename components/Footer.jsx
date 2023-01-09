57c2be940802f4a1008acff51af77c4e6b067183
MarianoElias03 <mariolovo76@hotmail.com>
2023-01-09 22:45:39 +1000

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,9 +8,9 @@ export default function Footer(){
                 <img className=' align-items-center circle rounded-circle ms-3' src='/PunchClean (1).png' width="50px" height="50px" />
             </a>
             <ul className='nav align-items-center justify-content-centre pb-3'>
-                <li className='nav-item'><a className='nav-link'>Home</a></li>
-                <li className='nav-item'><a className='nav-link'>About</a></li>
-                <li className='nav-item'><a className='nav-link'>Contact Me</a></li>
+                <li className='nav-item'><a className='nav-link' href='#'>Home</a></li>
+                <li className='nav-item'><a className='nav-link' href='#'>About</a></li>
+                <li className='nav-item'><a className='nav-link' href='#'>Contact Me</a></li>
             </ul>
         </div>
         </>
