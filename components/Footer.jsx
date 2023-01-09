9e486fd370fb1aa29d204048108fcf59d326f216
Mariano Elias Lovo <49789877+MarianoElias03@users.noreply.github.com>
2023-01-09 10:22:03 +1000

    logo update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,21 +3,15 @@ import React from 'react'
 export default function Footer(){
     return(
         <>
-        <div className='nav container-fluid bg-color-dark'>
-            <a>
-                <img src="" />
+        <div className='bg-dark navbar'>
+            <a className='navbar-brand'>
+                <img className=' align-items-center circle rounded-circle ms-3' src='/PunchClean (1).png' width="50px" height="50px" />
             </a>
-            <ul className='navbar-nav'>
+            <ul className='nav align-items-center justify-content-centre pb-3'>
                 <li className='nav-item'><a className='nav-link'>Home</a></li>
                 <li className='nav-item'><a className='nav-link'>About</a></li>
                 <li className='nav-item'><a className='nav-link'>Contact Me</a></li>
             </ul>
-            <a>
-                <form className=''>
-                    contact List
-                    <button>submit</button>
-                </form>
-            </a>
         </div>
         </>
     )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,14 +3,16 @@ import React from "react";
 export default function navbar(){
     return(
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-md bg-white z-3">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand p-0 m-0" >
+                        <img src="/PunchClean 1.svg" width="200px" height="100px"/>
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav  ">
                         <li className="nav-item">
                         <a class="nav-link" href="#">Home</a>
                         </li>
