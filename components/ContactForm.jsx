7aaf1ba858453eb2d565885e0aac30e4afba3425
Mariano Elias Lovo <49789877+MarianoElias03@users.noreply.github.com>
2023-01-11 10:30:53 +1000

    contact update

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-
+import styles from "../styles/Contact.module.css"
+import sendEmail from "../pages/api/sendEmail.js"
 export default function contactForm(){
     return(
         <>
+            <form className={styles.submitformcont}  method="post" onSubmit={sendEmail} id='emailform'>
+                  <label className={styles.fullnamelbl} htmlFor="name">Name:</label>
+                  <br/>
+                  <input className={styles.fullnamebtn} type="text" id="name" placeholder='Name'  name="name" />
+
+                  <br/>
+                  <br/>
+
+                  <label className={styles.emaillbl} htmlFor="mail">E-mail: <a className="text-danger fw-bold">*</a></label>
+                  <br/>
+                  <input className={styles.emailbtn}type="email" id="mail" name="email" placeholder="Enter your Email" required/>
+
+                  <br/>
+                  <br/>
+                  <label className={styles.textlbl}htmlFor="msg">Message:</label>
+                  <br/>
+                  <textarea className={styles.textbtn} id="msg" name="message"  placeholder="Message"></textarea>
+
+                  <br/>
+
+                  <button className={styles.submitbtn}> Submit </button>
+            </form>
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Script from 'next/script'
-import { Navbar } from "../components"
+import { ContactForm } from "../components"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,16 +19,17 @@ export default function Home() {
 
       <main className={styles.main}>
           <div className={styles.cover}>  
-            <a className='text-light text-capitalize text-semi-bold'>
-              <h1 className={styles.overlay}>
+            <a>
+              <h1 className='text-light text-capitalize text-semi-bold bg-secondary text-center align-center'>
                 Washing Services for your home and offices
               </h1>              
               <img src="/Example 2.jpg" className={styles.coverPhoto} />
-
             </a>
           </div>
           <div className={styles.banner}>
-            <div className={styles.skills}>
+            <div className="container">
+              <div className='row justify-content-md-center'>
+                <div className='card bg-secondary m-3 p-3 border-white border-3 col'>
                   <div className={styles.ben} id="Eco-Friendly">
                     <a className='text-light'>
                       Use eco-friendly cleaning products that will not affect your gardens 
@@ -41,6 +42,8 @@ export default function Home() {
                       </svg> 
                     </a>
                   </div>
+                </div>
+                <div className='card bg-secondary m-3 p-3 border-white border-3 col'>
                   <div className={styles.ben} id="Quality">
                     <a className='text-white text-semi-bold text-center'>
                       We provide top quality work and will make sure that those sufaces look brand new
@@ -53,6 +56,8 @@ export default function Home() {
                     </a>
                     <br/>
                   </div> 
+                </div>
+                <div className='card bg-secondary m-3 p-3 border-white border-3 col'>
                   <div className={styles.ben} id="Affordable">
                     <a className='text-white text-semi-bold'>
                       We provide affordable pricing to give you at the best rate for the highest quality
@@ -63,8 +68,17 @@ export default function Home() {
                         <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
                       </svg>
                     </a>
-              </div>       
+                  </div>
+                </div>     
+              </div>  
             </div>
+          </div>
+          <div className={styles.contact}>
+            <span className='fs-1 m-3 text-semi-bold'>Get a Quote Today!</span>
+            <div className={styles.contactform}>
+              <ContactForm />
+            </div>
+            
           </div>
       </main>
     </>
