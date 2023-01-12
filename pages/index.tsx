96741b4ee7009326efc0a115d04351e5d205b785
Mariano Elias Lovo <49789877+MarianoElias03@users.noreply.github.com>
2023-01-12 10:32:12 +1000

    banner update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import Script from 'next/script'
 import { ContactForm } from "../components"
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,24 +18,25 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-          <div className={styles.cover}>  
-            <a>
-              <h1 className='text-light text-capitalize text-semi-bold bg-secondary text-center align-center'>
-                Washing Services for your home and offices
-              </h1>              
-              <img src="/Example 2.jpg" className={styles.coverPhoto} />
-            </a>
+        <div className='container-fluid'>
+          <div className="card text-bg-dark rounded-0">
+            <img src="/Example 2.jpg" className="card-img vh-100 rounded-0" alt="..."/>
+            <div className="card-img-overlay row vh-100 container-fluid px-0 ms-0">
+              <h1 className="col-sm-12 my-auto card-title bg-dark text-center align-middle bg-opacity-75 text-capitalize" >Power Washing Services for your home and offices</h1>
+            </div>
           </div>
-          <div className={styles.banner}>
-            <div className="container">
-              <div className='row justify-content-md-center'>
-                <div className='card bg-secondary m-3 p-3 border-white border-3 col'>
-                  <div className={styles.ben} id="Eco-Friendly">
-                    <a className='text-light'>
-                      Use eco-friendly cleaning products that will not affect your gardens 
-                    </a>
+            <div className="container-fluid">
+              <div className='row bg-secondary'>
+
+                <div className='w-25 p-4 m-auto ms-3 align-middle card bg-secondary border-white border-2' id="Eco-friendly">
+                  <div className='row g-0'>
+                    <div className='col-8'>
+                      <a className='text-light card-body'>
+                        Use eco-friendly cleaning products that will not affect your gardens 
+                      </a>
+                    </div>  
                     <br/>
-                    <a className='justify-content-end align-items-end'>
+                    <a className='justify-content-end align-items-end col-4'>
                       <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="white" className="bi bi-globe-asia-australia" viewBox="3 0 10 16">
                         <path d="m10.495 6.92 1.278-.619a.483.483 0 0 0 .126-.782c-.252-.244-.682-.139-.932.107-.23.226-.513.373-.816.53l-.102.054c-.338.178-.264.626.1.736a.476.476 0 0 0 .346-.027ZM7.741 9.808V9.78a.413.413 0 1 1 .783.183l-.22.443a.602.602 0 0 1-.12.167l-.193.185a.36.36 0 1 1-.5-.516l.112-.108a.453.453 0 0 0 .138-.326ZM5.672 12.5l.482.233A.386.386 0 1 0 6.32 12h-.416a.702.702 0 0 1-.419-.139l-.277-.206a.302.302 0 1 0-.298.52l.761.325Z"/>
                         <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM1.612 10.867l.756-1.288a1 1 0 0 1 1.545-.225l1.074 1.005a.986.986 0 0 0 1.36-.011l.038-.037a.882.882 0 0 0 .26-.755c-.075-.548.37-1.033.92-1.099.728-.086 1.587-.324 1.728-.957.086-.386-.114-.83-.361-1.2-.207-.312 0-.8.374-.8.123 0 .24-.055.318-.15l.393-.474c.196-.237.491-.368.797-.403.554-.064 1.407-.277 1.583-.973.098-.391-.192-.634-.484-.88-.254-.212-.51-.426-.515-.741a6.998 6.998 0 0 1 3.425 7.692 1.015 1.015 0 0 0-.087-.063l-.316-.204a1 1 0 0 0-.977-.06l-.169.082a1 1 0 0 1-.741.051l-1.021-.329A1 1 0 0 0 11.205 9h-.165a1 1 0 0 0-.945.674l-.172.499a1 1 0 0 1-.404.514l-.802.518a1 1 0 0 0-.458.84v.455a1 1 0 0 0 1 1h.257a1 1 0 0 1 .542.16l.762.49a.998.998 0 0 0 .283.126 7.001 7.001 0 0 1-9.49-3.409Z"/>
@@ -44,12 +44,15 @@ export default function Home() {
                     </a>
                   </div>
                 </div>
-                <div className='card bg-secondary m-3 p-3 border-white border-3 col'>
-                  <div className={styles.ben} id="Quality">
-                    <a className='text-white text-semi-bold text-center'>
-                      We provide top quality work and will make sure that those sufaces look brand new
-                    </a>
-                    <a>
+
+                <div className='w-25 p-4 m-auto ms-3 align-middle card bg-secondary border-white border-2' id="Quality">
+                  <div className="row g-0" >
+                    <div className='col-8'>
+                      <a className='text-white text-semi-bold text-center card-body'>
+                        We provide top quality work and will make sure that those sufaces look brand new
+                      </a>
+                    </div>
+                    <a className="col-4">
                     <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="white" className="bi bi-check2-square" viewBox="3 0 10 16">
                       <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"/>
                       <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
@@ -59,7 +62,7 @@ export default function Home() {
                   </div> 
                 </div>
                 <div className='card bg-secondary m-3 p-3 border-white border-3 col'>
-                  <div className={styles.ben} id="Affordable">
+                  <div className="" id="Affordable">
                     <a className='text-white text-semi-bold'>
                       We provide affordable pricing to give you at the best rate for the highest quality
                     </a>
@@ -73,14 +76,13 @@ export default function Home() {
                 </div>     
               </div>  
             </div>
-          </div>
           <div className={styles.contact}>
             <span className='fs-1 m-3 text-semi-bold'>Get a Quote Today!</span>
             <div className={styles.contactform}>
               <ContactForm />
             </div>
-            
           </div>
+        </div>  
       </main>
     </>
   )
