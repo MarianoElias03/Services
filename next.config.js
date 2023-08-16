7b7a165a1d9d7b6934093c5913cd014cde84bbcf
MarianoElias03 <mariolovo76@hotmail.com>
2023-08-16 10:36:15 +1000

    purge unused CSS

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
+
+module.exports = withCss(withPurgeCss());
+
 module.exports = nextConfig
